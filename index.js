90b273894c8ca7428e48cff7cedc81d1b10a3091
danou-lua <popescumarin909@gmail.com>
2026-09-03 09:43:15 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+
+app.command("/mark-1-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+app.command("/mark-1-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/mark-1-help", async ({ command,ack, respond }) => {
+  await ack();
+  await respond({
+    text:`Available Commands:
+/mark-1-ping - Check bot latency
+/mark-1-catfact - Get a cat fact`
+  });
+});
+
+app.command("/mark-1-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
