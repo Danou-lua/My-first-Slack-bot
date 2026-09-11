2fd3f69150354ceb006d81292754aadd397ccbd5
danou-lua <popescumarin909@gmail.com>
2026-09-11 20:47:50 +0200

    joke command has been removed

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ const app = new App({
   socketMode: true
 });
 
-
-app.command("/mark-1-joke", async ({ ack, respond }) => {
-  await ack();
-
-  try {
-    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
-    await respond({
-      text:
-`${response.data.setup}
-
-${response.data.punchline}`
-    });
-  } catch (err) {
-    await respond({ text: "Failed to fetch a joke." });
-  }
-});
 let random;
 let request;
 let table;
